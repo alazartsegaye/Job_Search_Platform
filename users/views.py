a4b79a85bcfdcecfc79ceb39c484e2120e2ff04e
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-11 22:47:08 +0300

    Add user views for registration and authentication

--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from .models import User
+from .serializers import UserSerializer
+from rest_framework.views import APIView
+from rest_framework import generics, viewsets
+from django.contrib.auth import authenticate, login, logout
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+        return Response({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
