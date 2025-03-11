b24d54ec84e9834c1f76b52285e396cf105dde8d
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-11 22:21:14 +0300

    Configure database settings in settings.py for MySQL

--- job_search_api/settings.py
+++ job_search_api/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'job_search_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': BASE_DIR / 'jobquestapi_db',
+        'USER': 'root',
+        'PASSWORD': 'teregaga@001',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
