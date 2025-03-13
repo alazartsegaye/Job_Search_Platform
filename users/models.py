fee93a4be71f4e842799837b887f56d1d66c4b20
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-14 01:04:50 +0300

    Update User model to include additional fields

--- users/models.py
+++ users/models.py
@@ -5,6 +5,3 @@ class User(AbstractUser):
     phone_number = models.CharField(max_length=15, null=True)
     address = models.TextField(null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True)
-
-    def __str__(self):
-        return self.username
