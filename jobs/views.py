f968369f961e6116a982d7e54b76ac5fde11fcfe
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-14 01:28:27 +0300

    Add filtering functionality to Job listings

--- jobs/views.py
+++ jobs/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import JsonResponse
 from rest_framework import viewsets, permissions, generics
 from .models import Job, JobApplication
 from .serializers import JobSerializer, JobApplicationSerializer
-from django.contrib.auth.decorators import login_required
+from rest_framework import filters
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all().order_by('-posted_at')
     serializer_class = JobSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'company__name', 'location']
+
 
     def perform_create(self, serializer):
         serializer.save(posted_by=self.request.user)
 
-@login_required
 class ApplyJobView(generics.CreateAPIView):
     serializer_class = JobApplicationSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -22,13 +23,6 @@ class ApplyJobView(generics.CreateAPIView):
         job = get_object_or_404(Job, id=self.kwargs['job_id'])
         serializer.save(applicant=self.request.user, job=job)
 
-class JobCreateView(generics.CreateAPIView):
-    serializer_class = JobSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer):
-        serializer.save(posted_by=self.request.user)
-
 class JobApplicationListView(generics.ListAPIView):
     serializer_class = JobApplicationSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -36,7 +30,6 @@ class JobApplicationListView(generics.ListAPIView):
     def get_queryset(self):
         job = get_object_or_404(Job, id=self.kwargs['job_id'])
         return JobApplication.objects.filter(job=job)
-
 
 def job_listings_view(request):
     jobs = Job.objects.all().order_by('-posted_at')
