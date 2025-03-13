3f0d6409eb13e15aea99d859e50c89cfe34e81f7
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-14 01:02:46 +0300

    Add Job and JobApplication models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    company = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+class JobApplication(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, default=1)
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
+    applied_at = models.DateTimeField(auto_now_add=True)
+    cover_letter = models.TextField(blank=True, null=True)
