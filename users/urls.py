fc101e227812aa58ed3ff42e0693df4e11cc1131
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-16 01:14:15 +0300

    Modify urls.py to route requests to updated views using Django's HTML templates

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
-from .views import RegisterView, UserViewSet, LoginView, LogoutView
+from .views import UserViewSet, home_view, register_view, login_view, logout_view
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+
     path('', include(router.urls)),
+    path('', home_view, name='home'),
+    path('register/', register_view, name='register'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
