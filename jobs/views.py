bdf779f89ece3899a3aac286b082431a7baea1a2
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-17 22:23:55 +0300

    Update ApplyJobView to return response for unauthenticated users

--- jobs/views.py
+++ jobs/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework import viewsets, permissions, generics
-from .models import Job
 from .serializers import JobSerializer, JobApplicationSerializer
-from django.http import JsonResponse
-from django.views import View
+from .models import Job
+from rest_framework import viewsets, permissions, generics
 from rest_framework import filters
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all().order_by('-posted_at')
     serializer_class = JobSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'company__name', 'location']
+    filterset_fields = ['title', 'company', 'location']
 
 
     def perform_create(self, serializer):
@@ -25,6 +25,14 @@ class ApplyJobView(generics.CreateAPIView):
         job = get_object_or_404(Job, id=self.kwargs['job_id'])
         serializer.save(applicant=self.request.user, job=job)
 
+    def handle_exception(self, exc):
+        if isinstance(exc, PermissionDenied):
+            return Response(
+                {"detail": "Authentication required. Please log in or create an account to apply for this job."},
+                status=403
+            )
+        return super().handle_exception(exc)
+
 class JobCreateView(generics.CreateAPIView):
     serializer_class = JobSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -32,19 +40,12 @@ class JobCreateView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save(posted_by=self.request.user)
 
+class JobCreateView(generics.CreateAPIView):
+    serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-class JobDetailView(View):
-    def get(self, job_id):
-        try:
-            job = Job.objects.get(id=id)
-            data = {
-                'id': job.id,
-                'title': job.title,
-                'description': job.description,
-            }
-            return JsonResponse(data)
-        except Job.DoesNotExist:
-            return JsonResponse({'error': 'Job not found'}, status=404)
+    def perform_create(self, serializer):
+        serializer.save(posted_by=self.request.user)
 
 def job_listings_view(request):
     jobs = Job.objects.all()
