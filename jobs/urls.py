88aae6dd25c9e87008b100247a96ec678a119384
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-17 22:45:57 +0300

    Remove duplicate deltaview URL definition

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import JobViewSet, ApplyJobView, JobCreateView, job_listings_view, JobDetailView
+from .views import JobViewSet, ApplyJobView, JobCreateView, job_listings_view
 
 router = DefaultRouter()
 router.register(r'jobs', JobViewSet, basename='job')
 
 urlpatterns = [
+
     path('', include(router.urls)),
     path('listings/', job_listings_view, name='job_listings'),
     path('create/', JobCreateView.as_view(), name='create_job'),
-    path('apply/<int:job_id>/', ApplyJobView.as_view(), name='apply_job'),
-    path('jobs/<int:job_id>/', JobDetailView.as_view(), name='job_detail'),
+    path('<int:job_id>/apply/', ApplyJobView.as_view(), name='apply_job'),
 ]
