5ba7e4c9f4cd2225c03652a1b4d60dec308d94d7
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-17 22:42:26 +0300

    Optimize Job model field lengths

--- jobs/models.py
+++ jobs/models.py
@@ -4,21 +4,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Job(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=100)
     description = models.TextField()
-    company = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
+    company = models.CharField(max_length=150)
+    location = models.CharField(max_length=150)
     posted_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
             return self.title
 
 class JobApplication(models.Model):
-    job = models.ForeignKey(Job, on_delete=models.CASCADE, default=1)
-    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
-    applied_at = models.DateTimeField(auto_now_add=True)
-    cover_letter = models.TextField(blank=True, null=True)
-    resume = models.FileField(upload_to='resumes/', null=True, blank=True)
 
     class Status(models.TextChoices):
         PENDING = 'pending', 'Pending'
@@ -30,6 +25,12 @@ class JobApplication(models.Model):
         choices=Status.choices,
         default=Status.PENDING
     )
+
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, default=1)
+    applicant = models.ForeignKey(User, on_delete=models.CASCADE)
+    applied_at = models.DateTimeField(auto_now_add=True)
+    cover_letter = models.TextField(blank=True, null=True)
+    resume = models.FileField(upload_to='resumes/')
 
     def __str__(self):
         return f"{self.applicant.username} applied for {self.job.title}"
