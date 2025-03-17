ec9ab54ffccf294c2cb1bf174e8b58d6c2331fb3
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-17 22:43:15 +0300

    Update JobSerializer with read-only fields

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -4,12 +4,12 @@ from .models import Job, JobApplication
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
-        fields = ['id', 'title', 'description', 'company', 'location', 'posted_at']
-        read_only_fields = ['posted_at', 'posted_by']
+        fields = "__all__"
+        read_only_fields = ["id", "posted_at"]
 
 class JobApplicationSerializer(serializers.ModelSerializer):
     class Meta:
         model = JobApplication
         fields = ['id', 'job', 'applicant', 'applied_at', 'cover_letter', 'resume']
-        read_only_fields = ['applied_at', 'applicant']
+        read_only_fields = ['id', 'applied_at', 'applicant']
 
