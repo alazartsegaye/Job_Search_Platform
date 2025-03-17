cf73ac1083e94606cb7578f692f715f1378e4be0
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-17 23:51:16 +0300

    Configure URL path for API home view and media file serving in development mode

--- job_search_api/urls.py
+++ job_search_api/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', TemplateView.as_view(template_name='users/home.html'), name='home'),
     path('api/', include('users.urls')),
     path('api/', include('jobs.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
