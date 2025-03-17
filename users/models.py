d852516085e500554bf055afeb1de477a373de67
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-17 22:02:15 +0300

    Add string representation method to return username for User model

--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=15, null=True)
-    address = models.TextField(null=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
