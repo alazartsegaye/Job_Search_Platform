ddbc6622ada0d0d1a60bd17b0044e45feca39cdd
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-18 17:32:53 +0300

    Update urls.py to include CRUD paths for job lists

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import JobViewSet, ApplyJobView, JobCreateView, job_listings_view
+from .views import (JobListView, JobDetailView, JobCreateView, JobUpdateView, JobDeleteView, JobApplyView, job_listings_view)
 
 router = DefaultRouter()
-router.register(r'jobs', JobViewSet, basename='job')
+router.register(r'jobs', JobListView, basename='job')
 
 urlpatterns = [
 
     path('', include(router.urls)),
-    path('listings/', job_listings_view, name='job_listings'),
-    path('create/', JobCreateView.as_view(), name='create_job'),
-    path('<int:job_id>/apply/', ApplyJobView.as_view(), name='apply_job'),
+    path('job/lists/', job_listings_view, name='job_listings'),
+    path('job/<int:id>/', JobDetailView.as_view(), name='job-detail'),
+    path('job/create/', JobCreateView.as_view(), name='create_job'),
+    path('job/<int:id>/update/', JobUpdateView.as_view(), name='job-update'),
+    path('job/<int:id>/delete/', JobDeleteView.as_view(), name='job-delete'),
+    path('job/<int:id>/apply/', JobApplyView.as_view(), name='apply_job'),
 ]
