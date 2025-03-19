849e93ac38c08bb73813d38174258b377a2b0a48
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-20 00:18:50 +0300

    restrict job post updates and deletions to the employer

--- jobs/permissions.py
+++ jobs/permissions.py
@@ -1,8 +1,6 @@
 from rest_framework.permissions import BasePermission
-from rest_framework.views import View
-
 
 class IsEmployer(BasePermission):
+
     def has_object_permission(self, request, view, obj):
         return obj.employer == request.user
-    
