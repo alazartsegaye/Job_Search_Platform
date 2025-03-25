8274aff8990042ccfc6716ccb4a28f59e603c40a
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-25 22:38:47 +0300

    Add properties to User model for role identification

--- users/models.py
+++ users/models.py
@@ -2,9 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    phone_number = models.CharField(max_length=16, null=True, blank=True, unique=True)
     address = models.TextField(null=True, blank=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+    @property
+    def is_employer(self):
+        return self.jobs.exists()
+
+    @property
+    def is_applicant(self):
+        return self.jobapplication_set.exists()
 
     def __str__(self):
         return self.username
