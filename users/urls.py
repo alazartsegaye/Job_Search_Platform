051677121735a155c15924a210bf0c8ac3ccae20
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-30 23:31:34 +0300

    Add URL path for user profile view

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,14 @@
-from django.urls import path, include
-from .views import UserViewSet, home_view, register_view, login_view, logout_view
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import UserViewSet, home_view, register_view, login_view, UserProfileView
 
 urlpatterns = [
-
-    path('', include(router.urls)),
-    path('', home_view, name='home'),
+    path('home/', home_view, name='home'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
+    path('user/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user-detail'),
+
 ]
