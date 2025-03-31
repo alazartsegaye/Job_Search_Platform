8166d22b672010ffcb6a43a913d9393b8d8baa42
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-31 19:50:52 +0300

    Add URL path for applicants job application View

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,17 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (JobListView, JobDetailView, JobCreateView, JobUpdateView, JobDeleteView, JobApplyView, job_listings_view)
+from .views import (JobView, JobApplyView, job_listings_view, JobApplicationListView, JobCreateView,
+                    JobApplicationResponseView, ApplicantJobApplicationView )
 
 router = DefaultRouter()
-router.register(r'jobs', JobListView, basename='job')
+router.register(r'jobs', JobView, basename='job')
 
 urlpatterns = [
 
     path('', include(router.urls)),
     path('job/lists/', job_listings_view, name='job_listings'),
-    path('job/<int:id>/', JobDetailView.as_view(), name='job-detail'),
-    path('job/create/', JobCreateView.as_view(), name='create_job'),
-    path('job/<int:id>/update/', JobUpdateView.as_view(), name='job-update'),
-    path('job/<int:id>/delete/', JobDeleteView.as_view(), name='job-delete'),
-    path('job/<int:id>/apply/', JobApplyView.as_view(), name='apply_job'),
+    path('job/create/', JobCreateView.as_view(), name='job-create'),
+    path('job/apply/', JobApplyView.as_view(), name='apply_job'),
+    path('job/application/', JobApplicationListView.as_view(), name='employer-applications'),
+    path('application/<int:pk>/respond/', JobApplicationResponseView.as_view(), name='respond-application'),
+    path('applicant/application/', ApplicantJobApplicationView.as_view(), name='applicant-applications'),
 ]
