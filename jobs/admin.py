912a8474862a3274446d4452f0b5bb6438a59757
alazartsegaye <alazartsegaye0@gmail.com>
2025-03-31 19:32:17 +0300

    Enhance admin interface for Job and JobApplication models

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Job, JobApplication
 
-admin.site.register(Job)
-admin.site.register(JobApplication)
+class JobApplicationInline(admin.TabularInline):
+    model = JobApplication
+    extra = 0
+    readonly_fields = ['applied_at']
+
+@admin.register(Job)
+class JobAdmin(admin.ModelAdmin):
+    list_display = ['title', 'company', 'location', 'posted_at']
+    list_filter = ['posted_at']
+    search_fields = ['title', 'company', 'description']
+    inlines = [JobApplicationInline]
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ['job', 'applicant', 'applied_at']
+    list_filter = ['status', 'applied_at']
+    search_fields = ['job__title', 'applicant__username']
