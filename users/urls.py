895358df0bf4726f8e0e195476771c264ffa77bf
alazartsegaye <alazartsegaye0@gmail.com>
2025-04-02 12:47:40 +0300

    Update URLs to use API routes for login, logout, and profile views

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from .views import UserViewSet, home_view, register_view, login_view, UserProfileView
+from .views import (
+    UserViewSet, home_view, RegisterAPIView, LoginAPIView, LogoutAPIView, UserProfileView
+)
 
 urlpatterns = [
     path('home/', home_view, name='home'),
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('register/', RegisterAPIView.as_view(), name='register'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('users/', UserViewSet.as_view({'get': 'list', 'post': 'create'}), name='user-list'),
     path('user/<int:pk>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user-detail'),
-
 ]
